be8705c9fcceb614d03bf55838d1c984bad86e8a
romanbishof <romanbishof@gmail.com>
2021-08-22 16:50:36 +0300

    back end update

--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
-const express = require('express')
-const socketio = require('socket.io')
+const express = require('express');
+const socketio = require('socket.io');
 
 // build in module
-const http = require('http')
+const http = require('http');
 
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router');
 
 // setting up the socket.io
 const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+// using the build in method to register clients joining and leving our app
+io.console('connection', (socket) => {
+    console.log(('We have new connection!!!'));
+
+    socket.on('disconnect', () => {
+        console.log('User had left!!!');
+    })
+})
+
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`))
+ 
